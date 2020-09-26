3a24ad46af0b0d920735eafed8b8e5bf012866aa
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-09-26 17:52:27 +0530

    login

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <h2>Login</h2>
+        <label>User Name</label>
+        <input type="text" name="uname" placeholder="User name"> <br><br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password"> <br><br>
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
